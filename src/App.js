c1325ddb0e47a0ca773ba30131cdad0e11b1e9fe
T. Rahaman <tanvirtex@gmail.com>
2020-08-28 06:40:56 +0600

    build_react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p> Ǒmega Protocol |2019-2020 </p>
+    </div>
+  );
+}
+
+export default App;
